d5ab1c148275017e469a3135d041933187d9ae58
Layyoub <ayoubbenmed46@gmail.com>
2026-04-07 18:20:26 +0100

    feat: implement global Messenger-style video call system and cleanup prototype

--- Front_End/CODE-REACT/src/App.jsx
+++ Front_End/CODE-REACT/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React from "react";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n/i18n.js";
 
@@ -8,7 +8,7 @@ import { AuthProvider } from "./context/AuthContext";
 import { HandGestureProvider } from "./context/HandGestureContext";
 import HandGestureOverlay from "./components/HandGestureOverlay";
 import VirtualKeyboard from "./components/VirtualKeyboard";
-import VideoCallModal from './components/VideoCallModal';
+import GlobalCallLayer from "./components/GlobalCallLayer";
 
 // import state selectors
 import {
@@ -19,10 +19,6 @@ import {
 function App({ children }) {
   const dispatch = useDispatch();
   dispatch(setSetting());
-  // Example state for demo/testing
-  const [showVideo, setShowVideo] = useState(false);
-  const selfId = 'user1'; // Replace with logged-in user ID
-  const peerId = 'user2'; // Replace with selected user ID
 
   return (
     <I18nextProvider i18n={i18n}>
@@ -31,8 +27,7 @@ function App({ children }) {
           <div className="App">{children}</div>
           <HandGestureOverlay />
           <VirtualKeyboard />
-          <button onClick={() => setShowVideo(true)}>Test Video Call</button>
-          <VideoCallModal show={showVideo} onClose={() => setShowVideo(false)} selfId={selfId} peerId={peerId} />
+          <GlobalCallLayer />
         </HandGestureProvider>
       </AuthProvider>
     </I18nextProvider>
